ce82cb9cc36cc6a949510fd9e132eb9c6787f73d
Glagny <nikoicardo@gmail.com>
2021-10-18 14:49:11 -0600

    Attempt to please Heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -9,6 +10,17 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/restaurants', require('./routes/restaurants'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  // If the home page is accessed, load the index html file.
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
