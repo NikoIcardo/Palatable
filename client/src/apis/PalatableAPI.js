cab13989e078e8a4f282dc764b257d200a3914bf
Glagny <nikoicardo@gmail.com>
2021-10-18 15:01:56 -0600

    Attempt to please Heroku

--- client/src/apis/PalatableAPI.js
+++ client/src/apis/PalatableAPI.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/api/v1/restaurants',
+  baseURL:
+    process.env.baseURL + '/api/v1/restaurants' ||
+    'http://localhost:5000/api/v1/restaurants',
 });
